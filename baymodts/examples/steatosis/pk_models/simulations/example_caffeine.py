fb2e06dc0e6b7543ebebe8fb239ad975bc9afe25
Matthias Koenig <konigmatt@googlemail.com>
2023-12-13 15:30:45 +0100

    starting caffeine example

---
+++ baymodts/examples/steatosis/pk_models/simulations/example_caffeine.py
@@ -0,0 +1,57 @@
+"""Example simulations for caffeine model."""
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import roadrunner
+from matplotlib import pyplot as plt
+from sbmlutils.console import console
+
+labels = {
+    "caf_gut": "caffeine (gut)",
+    "caf_cent": "caffeine (plasma)",
+    "caf_peri": "caffeine (peripheral)",
+}
+
+def reference_simulation(r: roadrunner.RoadRunner) -> None:
+    """Reference simulation."""
+
+    console.rule("Caffeine simulation", style="white")
+    f, ax = plt.subplots(
+        nrows=1, ncols=1, figsize=(5, 5), dpi=300,
+        layout="constrained"
+    )
+
+    # simulation
+    r.resetAll()
+    s = r.simulate(start=0, end=10, steps=400)  # [min]
+    df: pd.DataFrame = pd.DataFrame(s, columns=s.colnames)
+
+    for sid in ["caf_gut", "caf_cent", "caf_peri"]:
+        linewidth = 1.0
+        if sid == "caf_cent":
+            linewidth = 2.0
+
+        ax.plot(
+            df.time/60,  # [min] -> [hr]
+            df[sid],
+            label=sid,
+            linewidth=linewidth,
+        )
+
+        # ax.legend()
+        ax.set_title("Reference simulation", fontweight="bold", fontsize="15")
+        ax.set_xlabel("time [min]", fontweight="bold", fontsize="12")
+        ax.set_ylabel("concentration [mmole/min]", fontweight="bold", fontsize="12")
+
+    ax.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    # load_model
+    from pk_models import MODELS_DIR
+    simple_pk_path: Path = MODELS_DIR / "caffeine_pk.xml"
+    r: roadrunner.RoadRunner = roadrunner.RoadRunner(str(simple_pk_path))
+    reference_simulation(r)
